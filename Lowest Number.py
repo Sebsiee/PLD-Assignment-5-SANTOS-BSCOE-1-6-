63b739c74291ca83cca52b44a0a2fd6c113e0521
Sebastian <santosbasty4@gmail.com>
2021-11-13 20:33:03 +0800

    Second Commit - Loops and Function Added

--- Lowest Number.py
+++ Lowest Number.py
@@ -2,15 +2,22 @@ def introduction():
     print("Welcome!")
 
 def lowestNumber():
-    firstInput = int(input("Input 1st Number: "))
-    secondInput = int(input("Input 2nd Number: "))
-    thirdInput = int(input("Input 3rd Number: "))
+    while True:
+        try:
+            firstInput = int(input("Input 1st Number: "))
+            secondInput = int(input("Input 2nd Number: "))
+            thirdInput = int(input("Input 3rd Number: "))
+            break
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+            continue
+    print(f"Your three numbers are: {firstInput}, {secondInput}, {thirdInput}")
     if firstInput < secondInput and thirdInput:
-        print("The lowest number is ", firstInput)
-    if secondInput < firstInput and thirdInput:
-        print("The lowest number is ", secondInput)
-    if thirdInput < firstInput and secondInput:
-        print("The lowest number is ", thirdInput)
+        print("The lowest number is: ", firstInput)
+    elif secondInput < firstInput and thirdInput:
+        print("The lowest number is: ", secondInput)
+    elif thirdInput < firstInput and secondInput:
+        print("The lowest number is: ", thirdInput)
 
 introduction()
 lowestNumber()
