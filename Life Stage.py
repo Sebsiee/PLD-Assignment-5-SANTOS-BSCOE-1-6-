606c00f50bee6c5827bbf7ec5fa5b2b32aba7ad0
Sebastian <santosbasty4@gmail.com>
2021-11-13 20:39:17 +0800

    Second Commit - Loops Added

--- Life Stage.py
+++ Life Stage.py
@@ -2,15 +2,21 @@ def introduction():
     print("Welcome!")
 
 def lifeStage():
-    agePerson = int(input("How old are you? "))
-    if agePerson >= 0 and agePerson <= 12:
-        print("You're a Kid. How Cute!")
-    elif agePerson >= 13 and agePerson <= 17:
-        print("You're a Teen. How Cool!")
-    elif agePerson == 18:
-        print("You're a Debutant. How Nice!")   
-    elif agePerson >= 19:
-        print("You're an Adult. How Resoponsible!")
+    while True:
+        try:
+            agePerson = int(input("How old are you? "))
+        except ValueError:
+            print("Enter a Number.")
+            continue
+        if agePerson >= 0 and agePerson <= 12:
+            print("You're a Kid. How Cute!")
+        elif agePerson >= 13 and agePerson <= 17:
+            print("You're a Teen. How Cool!")
+        elif agePerson == 18:
+            print("You're a Debutant. How Nice!")   
+        elif agePerson >= 19:
+            print("You're an Adult. How Resoponsible!")
+        break
 
 introduction()
 lifeStage()
