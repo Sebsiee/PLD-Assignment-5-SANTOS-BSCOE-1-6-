9c065c6a0cacfe86e6c734dc96274b2738542b00
Sebastian <santosbasty4@gmail.com>
2021-11-13 21:42:07 +0800

    Third Commit - Polished, Added if Statement

--- Lowest Number.py
+++ Lowest Number.py
@@ -1,16 +1,34 @@
 def introduction():
-    print("Welcome!")
+    print("Welcome to my Program 2 - Lowest Number!")
 
 def lowestNumber():
     while True:
         try:
             firstInput = int(input("Input 1st Number: "))
-            secondInput = int(input("Input 2nd Number: "))
-            thirdInput = int(input("Input 3rd Number: "))
-            break
         except ValueError:
             print("Sorry, I didn't understand that.")
             continue
+        else:
+            break
+
+    while True:
+        try:
+            secondInput = int(input("Input 2nd Number: "))
+        except ValueError:  
+            print("Sorry, I didn't understand that.")
+            continue
+        else:
+            break
+
+    while True:
+        try:
+            thirdInput = int(input("Input 3rd Number: "))
+        except ValueError:  
+            print("Sorry, I didn't understand that.")
+            continue
+        else:
+            break
+        
     print(f"Your three numbers are: {firstInput}, {secondInput}, {thirdInput}")
     if firstInput < secondInput and thirdInput:
         print("The lowest number is: ", firstInput)
@@ -18,6 +36,13 @@ def lowestNumber():
         print("The lowest number is: ", secondInput)
     elif thirdInput < firstInput and secondInput:
         print("The lowest number is: ", thirdInput)
+    elif firstInput == secondInput == thirdInput:
+        print("You entered 3 identical numbers so there's no lowest nor highest number!")
+
+def goodbye():
+    print()
+    print("Thank you for using my program!")
 
 introduction()
 lowestNumber()
+goodbye()
